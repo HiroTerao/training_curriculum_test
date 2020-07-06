a9af8bf6eea9e665925fc42adacdbdd8cd36962c
HiroTerao <67026468+HiroTerao@users.noreply.github.com>
2020-07-06 17:03:43 +0900

    change_wday_method

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -12,7 +12,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def get_week
@@ -33,8 +33,8 @@ class CalendarsController < ApplicationController
         plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-      days = { month: (@todays_date + x).month, date: @todays_date.day + x, plans: plans, day: wdays[ (@todays_date + x).strftime('%w').to_i]}
-
+      days = { month: (@todays_date + x).month, date: @todays_date.day + x, plans: plans, day: wdays[ (@todays_date + x).wday]}
+      
       @week_days.push(days)
     end
   end
